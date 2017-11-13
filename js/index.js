7e51e144bf6b8d155b104070a0f1aae3601ac884
LeventeBito <BITL1_16@uni.worc.ac.uk>
2017-11-13 11:51:51 +0000

    final

--- js/index.js
+++ js/index.js
@@ -16,13 +16,15 @@ function random() {
 	var val = !Math.round(Math.random());
      alert(val);
     if (val == true) {
-        alert("randomTrew");
+ 
     navigator.notification.beep(1);
+        navigator.vibrate(2000);
        
         }
     else {
-        alert("randomFalse")
+   
     navigator.notification.beep(2);
+        navigator.vibrate([200, 300, 500]);
        
         }
 }
