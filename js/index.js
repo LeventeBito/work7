c35bcbaef0ac4558ad9fb6f1468bb4f5f8249875
LeventeBito <BITL1_16@uni.worc.ac.uk>
2017-11-13 11:14:47 +0000

    new

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-function random() {
-	var val = !Math.round(Math.random());
-     alert(val);
-    if (val == true) {
-    navigator.notification.beep(1);
-       
-        }
-    if (val == false) {
-    navigator.notification.beep(2);
-       
-        }
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function onDeviceReady(){
+    
+    alert("something");
+    $('#submitbutton').on("click",getAnswer);   
 }
+
+function getAnswer(){
+    
+    alert("button clicked");
+}
+
+//function random() {
+//	var val = !Math.round(Math.random());
+//     alert(val);
+//    if (val == true) {
+//    navigator.notification.beep(1);
+//       
+//        }
+//    if (val == false) {
+//    navigator.notification.beep(2);
+//       
+//        }
+//}
