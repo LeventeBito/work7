86a925e9461644c2a6b3009cb43219fe2bdb589d
LeventeBito <BITL1_16@uni.worc.ac.uk>
2017-11-13 10:21:03 +0000

    test1

--- js/index.js
+++ js/index.js
@@ -1,18 +1,4 @@
-<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
-
-$(document).on("pagecreate","#pageone",function(){
-  
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
-
-	$('#taptext').on("tap",function(){
-    	$(this).css('color', 'red');
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
-
-
-});
+function random() {
+	var val = !Math.round(Math.random());
+    alert(val);
+}
